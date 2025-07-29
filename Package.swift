50c29b9a847dfa6e7191509c403259857faa9b94
zach wick <zach@btrbet.app>
2025-07-29 14:42:22 -0400

    Adds swiftlint as an SPM dep and enforces rules (#17)
    * Initial swiftlint config
    
    Signed-off-by: zach wick <zach@btrbet.app>
    
    * Removes trailing whitespace
    
    Signed-off-by: zach wick <zach@btrbet.app>
    
    * Setting up git hooks to use swiftlint
    
    Signed-off-by: zach wick <zach@btrbet.app>
    
    * Adds git hook setup to dev env docs
    
    Signed-off-by: zach wick <zach@btrbet.app>
    
    * Fixes swiftlint warnings and errors
    
    Signed-off-by: zach wick <zach@btrbet.app>
    
    ---------
    
    Signed-off-by: zach wick <zach@btrbet.app>

--- Package.swift
+++ Package.swift
@@ -5,11 +5,15 @@ import PackageDescription
 
 let package = Package(
     name: "genie",
+    platforms: [
+        .macOS(.v10_15)
+    ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
         .package(url: "https://github.com/stephencelis/SQLite.swift.git", from: "0.15.4"),
         .package(url: "https://github.com/dastrobu/argtree.git", from: "1.5.2"),
+        .package(url: "https://github.com/realm/SwiftLint.git", from: "0.59.1"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
